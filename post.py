aa7cc92e876027138af32c74e2c07b23b89772ec
1z0ldyck <lucase2040@gmail.com>
2021-12-10 01:59:52 -0300

    feat: print status code and response of api post

--- post.py
+++ post.py
@@ -11,6 +11,4 @@ usuario = {
 # post person data in api
 r = requests.post("http://localhost:5000/post_people", data=json.dumps(usuario))
 
-# print message if get error in api
-if(r.status_code == 500):
-  print(r.text)
+print(f"Status code: {r.status_code}, message: {r.text}")
