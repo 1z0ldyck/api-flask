516f838e964e208e72fe8b48291717f9cda2f029
1z0ldyck <lucase2040@gmail.com>
2021-12-11 00:33:39 -0300

    chore!: change post.py to remove unnecessary code

--- post.py
+++ post.py
@@ -10,17 +10,6 @@ usuario = {
   }
 }
 
-# values = list(usuario.items())
-
-# key, value = values[0]
-# string = ''
-# k = list(value.keys())
-# v = list(value.values())
-
-
-
-# print(','.join(str(x) for x in k), ','.join(str(x) for x in v))
-
 # post person data in api
 r = requests.post("http://localhost:5000/post_person", data=json.dumps(usuario))
 
