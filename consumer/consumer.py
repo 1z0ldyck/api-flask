bd68c6ba6eda0c1141d44212eace4415af2e5d4b
1z0ldyck <lucase2040@gmail.com>
2021-12-11 00:29:40 -0300

    chore!: change publish_in_db method to accept type of table  & columns and values

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,38 +1,49 @@
-from dotenv import load_dotenv
-
-import pika, os, json
+import pika
+import json
 import psycopg2
-import consumer.config as config
+import config as config
+
 
 class Consumer:
-  """Application with the consumption proposal as application messages through the RabbitMQ queue"""
-  config = {}
-  
-  def __init__(self):
-    self.consumer_config = config.init_app(Consumer.config)
-    self.connection = pika.BlockingConnection(pika.ConnectionParameters(self.config['RABBITMQ_HOST']))
-    self.channel = self.connection.channel()
+    """Application with the consumption proposal as application messages through the RabbitMQ queue"""
+    config = {}
 
-  def receive_data(self):
-    """Consumes data from the RabbitMQ queue"""
-    self.channel.basic_consume(queue='send_message',
-                                 auto_ack=True,
-                                 on_message_callback=self.publish_in_db)
-    self.channel.start_consuming()
-  
-  @staticmethod
-  def publish_in_db(ch, method, properties, data):
-    """Publish the consumed data to the database"""
-    data_dict = json.loads(data)
-    conn_db = psycopg2.connect(host=Consumer.config['POSTGRES_HOST'], 
-                               database=Consumer.config['POSTGRES_DATABASE'], 
-                               user=Consumer.config['POSTGRES_USER'], 
-                               password=Consumer.config['POSTGRES_PASSWORD'])
-    cursor = conn_db.cursor()
-    cursor.execute(f""" 
-                   INSERT INTO People (name, age) VALUES ('{data_dict['name']}', {data_dict['age']})
-                   """)
-    conn_db.commit()
-    
+    def __init__(self):
+        self.consumer_config = config.init_app(Consumer)
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(self.config['RABBITMQ_HOST']))
+        self.channel = self.connection.channel()
+
+    def receive_data(self):
+        """Consumes data from the RabbitMQ queue"""
+        self.channel.basic_consume(queue='send_message',
+                                   auto_ack=True,
+                                   on_message_callback=self.publish_in_db)
+        self.channel.start_consuming()
+        
+    @staticmethod
+    def convert_str_to_int(values):
+      for value in values:
+        if not value.isalpha():
+          value = int(value)
+
+    @staticmethod
+    def publish_in_db(ch, method, properties, data):
+        """Publish the consumed data to the database"""
+        queue_data = list(json.loads(data).items())
+
+        conn_db = psycopg2.connect(host=Consumer.config['POSTGRES_HOST'],
+                                   database=Consumer.config['POSTGRES_DATABASE'],
+                                   user=Consumer.config['POSTGRES_USER'],
+                                   password=Consumer.config['POSTGRES_PASSWORD'])
+        cursor = conn_db.cursor()
+        for column, values in queue_data:
+            k, v = list(values.keys()), list(values.values())
+            cursor.execute(f""" 
+                    INSERT INTO {column} ({','.join(str(x) for x in k)}) VALUES ({(','.join("'" + str(x) + "'" for x in v))})
+                    """)
+            conn_db.commit()
+          
+          
 consumer = Consumer()
 consumer.receive_data()
--- post.py
+++ post.py
@@ -4,11 +4,24 @@ import requests
 import json
 
 usuario = {
-  "name": "Lucas",
-  "age": 20
+  "People": {
+    "name": "Lucas",
+    "age": 20
+  }
 }
 
+# values = list(usuario.items())
+
+# key, value = values[0]
+# string = ''
+# k = list(value.keys())
+# v = list(value.values())
+
+
+
+# print(','.join(str(x) for x in k), ','.join(str(x) for x in v))
+
 # post person data in api
-r = requests.post("http://localhost:5000/post_people", data=json.dumps(usuario))
+r = requests.post("http://localhost:5000/post_person", data=json.dumps(usuario))
 
 print(f"Status code: {r.status_code}, message: {r.text}")
